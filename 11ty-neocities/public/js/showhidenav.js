2c497a3508e0a120aed00e0046ce82650cb0f09f
KeigaI21 <keigaishibashi@gmail.com>
2026-08-07 00:21:55 -0700

    reformatted navigation, updated bio, cleaned up page layouts

--- 11ty-neocities/public/js/showhidenav.js
+++ 11ty-neocities/public/js/showhidenav.js
@@ -1,23 +1,36 @@
+const mediaQuery = window.matchMedia('(max-width: 600px)');
 var expandingButtons = document.querySelectorAll('.expanding-button');
-var showButton = document.querySelector('#show-button');
-var hideButton = document.querySelector('#hide-button');
+var navHamburger = document.querySelector('.nav-hamburger');
+var navClose = document.querySelector('.nav-close');
+var navPopup = document.querySelector('.nav-popup');
 var nav = document.querySelector('.nav');
-console.log(expandingButtons)
+
+function handleScreenChange(e) {
+    if (e.matches) {
+        hideNav()
+
+        navPopup.style.backgroundColor = "var(--primary-color)";
+        navPopup.style.borderStyle = "solid";
+        navPopup.style.borderColor = "var(--secondary-color)";
+    } else {
+        navPopup.style.display = "flex";
+        navHamburger.style.display = "none";
+
+        navClose.style.display = "none";
+        navPopup.style.backgroundColor = "transparent";
+        navPopup.style.borderStyle = "none";
+    }
+}
+// Dynamically adjusts to narrow screens by showing hamburger menu
+handleScreenChange(mediaQuery);
+mediaQuery.addListener(handleScreenChange);
 function showNav() {
-    expandingButtons.forEach((element) => {
-        element.classList.add("showing");
-    });
-    showButton.style.display = "none";
-    hideButton.style.display = "flex";
-    nav.style.backgroundColor = "var(--primary-color)";
-    nav.style.borderStyle = "none solid solid none";
+    navPopup.style.display = "flex";
+    navHamburger.style.display = "none";
+    navClose.style.display = "flex";
 }
 function hideNav() {
-    expandingButtons.forEach((element) => {
-        element.classList.remove("showing");
-    });
-    showButton.style.display = "flex";
-    hideButton.style.display = "none";
-    nav.style.backgroundColor = "var(--transparent)";
-    nav.style.borderStyle = "none";
+    navPopup.style.display = "none";
+    navHamburger.style.display = "flex";
+    navClose.style.display = "none";
 }
